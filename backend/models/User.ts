a6c9de82b653806a7a0e1170051fc2021006c34b
sakutaku <saikalabytova1@gmail.com>
2023-10-27 16:46:17 +0600

    TPC-2 added email field

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -40,6 +40,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    email: {
+        type: String,
+        required: true,
+    },
     role: {
         type: String,
         required: true,
--- backend/type.d.ts
+++ backend/type.d.ts
@@ -4,6 +4,7 @@ export interface IUser {
     token: string;
     role: string;
     displayName: string;
+    email: string;
     appleID?: string;
     googleID?: string;
 }
