9da3f9ba233fbe6f590583f582c9ffcd3e6a6494
Beka <122819000+BekatheRazzakov@users.noreply.github.com>
2023-10-28 17:18:52 +0600

    TPC-4: added login route
    The route verifies user by username and password, then gives the user data as response if user is verified.

--- backend/routers/users.ts
+++ backend/routers/users.ts
@@ -27,6 +27,31 @@ usersRouter.post("/", async (req, res, next) => {
     }
 });
 
+usersRouter.post("/sessions", async (req, res, next) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+
+        if (!user) {
+            return res.status(400).send({ error: "Wrong password or username!" });
+        }
+
+        const isMatch = await user.checkPassword(req.body.password);
+
+        if (!isMatch) {
+          return res.status(400).send({ error: "Wrong password or username!" });
+        }
+
+        user.generateToken();
+        await user.save();
+
+        res.send(user);
+    } catch (e) {
+      if (e instanceof mongoose.Error.ValidationError) {
+          return res.status(400).send(e);
+      }
+      return next(e);
+  }
+});
 
 usersRouter.delete("/sessions", auth, async (req, res) => {
     try {
