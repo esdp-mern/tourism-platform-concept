670592157876d46ee7c5196c93f55a4428f31a6e
Bektur Razzakov <brzzkv@gmail.com>
2023-10-30 20:28:03 +0600

    TPC-12: installed eslint in backend part

--- backend/models/Guid.ts
+++ backend/models/Guid.ts
@@ -1,28 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import User from "./User";
 
 const GuidSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-        required: true,
-        validate: {
-            validator: async (value: mongoose.Types.ObjectId) => await User.findById(value),
-            message: 'User does not exist!',
-        },
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+    validate: {
+      validator: async (value: mongoose.Types.ObjectId) =>
+        await User.findById(value),
+      message: "User does not exist!",
     },
-    languages: {
-        type: Array,
-        required: true,
-    },
-    country: {
-        type: String,
-        required: true,
-    },
-    rating: {
-        type: Array,
-    },
+  },
+  languages: {
+    type: Array,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Array,
+  },
 });
 
-const Guid = mongoose.model('Guid', GuidSchema);
+const Guid = mongoose.model("Guid", GuidSchema);
 export default Guid;
