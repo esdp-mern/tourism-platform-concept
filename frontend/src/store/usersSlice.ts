4aeb9aaf151db25a74026f306bc0e88b523ce8af
Bektur Razzakov <brzzkv@gmail.com>
2023-10-30 18:24:06 +0600

    TPC-8: new cases are added to extra reducers in usersSlice.ts

--- frontend/src/store/usersSlice.ts
+++ frontend/src/store/usersSlice.ts
@@ -1,43 +1,64 @@
 import {User, ValidationError} from "../type";
 import {createSlice} from "@reduxjs/toolkit";
-import {register} from "./usersThunk";
+import {register, signUp} from "./usersThunk";
 import {RootState} from "../app/store";
 
 interface UsersState {
-    user: User | null;
-    registerLoading: boolean;
-    registerError: ValidationError | null;
+  user: User | null;
+  registerLoading: boolean;
+  registerError: ValidationError | null;
+  signUpLoading: boolean;
+  signUpError: ValidationError | null;
 }
 
 const initialState: UsersState = {
-    user: null,
-    registerLoading: false,
-    registerError: null,
+  user: null,
+  registerLoading: false,
+  registerError: null,
+  signUpLoading: false,
+  signUpError: null,
 };
 
 export const usersSlice = createSlice({
-    name: "users",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(register.pending, (state) => {
-            state.registerLoading = true;
-            state.registerError = null;
-        });
-        builder.addCase(register.fulfilled, (state, { payload: userResponse }) => {
-            state.registerLoading = false;
-            state.user = userResponse.user;
-        });
-        builder.addCase(register.rejected, (state, { payload: error }) => {
-            state.registerLoading = false;
-            state.registerError = error || null;
-        });
-    },
+  name: "users",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(register.pending, (state) => {
+      state.registerLoading = true;
+      state.registerError = null;
+    });
+    builder.addCase(register.fulfilled, (state, {payload: userResponse}) => {
+      state.registerLoading = false;
+      state.user = userResponse.user;
+    });
+    builder.addCase(register.rejected, (state, {payload: error}) => {
+      state.registerLoading = false;
+      state.registerError = error || null;
+    });
+
+    builder.addCase(signUp.pending, (state) => {
+      state.signUpLoading = true;
+      state.signUpError = null;
+    });
+    builder.addCase(signUp.fulfilled, (state, {payload: userResponse}) => {
+      state.signUpLoading = false;
+      state.user = userResponse.user;
+    });
+    builder.addCase(signUp.rejected, (state, {payload: error}) => {
+      state.signUpLoading = false;
+      state.signUpError = error || null;
+    });
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
 export const selectUser = (state: RootState) => state.users.user;
 export const selectRegisterLoading = (state: RootState) =>
-    state.users.registerLoading;
+  state.users.registerLoading;
 export const selectRegisterError = (state: RootState) =>
-    state.users.registerError;
+  state.users.registerError;
+export const selectSignUpLoading = (state: RootState) =>
+  state.users.signUpLoading;
+export const selectSignUpError = (state: RootState) =>
+  state.users.signUpError;
