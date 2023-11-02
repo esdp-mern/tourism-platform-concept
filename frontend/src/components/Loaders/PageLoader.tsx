64699eab9f17b2ecb16eccf106a45f902e6058c3
Bektur Razzakov <brzzkv@gmail.com>
2023-11-02 23:33:55 +0600

    TPC-32: page loader is updated

--- frontend/src/components/Loaders/PageLoader.tsx
+++ frontend/src/components/Loaders/PageLoader.tsx
@@ -1,33 +1,14 @@
-import React from "react";
-import "./pageLoader.css";
-import { useAppSelector } from "../../app/hook";
-import { selectFetchAllLoading } from "../../store/toursSlice";
+import React from 'react';
+import './pageLoader.css';
+import { useAppSelector } from '../../app/hook';
+import { selectFetchAllLoading } from '../../store/toursSlice';
 
 const PageLoader = () => {
   const pageLoading = useAppSelector(selectFetchAllLoading);
 
   return (
-    <div className={`page-loader ${pageLoading && "enabled"}`}>
-      <div className="page-loader-inner">
-        <div className="page-loader-top">
-          <div className="loader-sun">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-        <div className="page-loader-bottom">
-          <span />
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
+    <div className={`page-loader ${pageLoading && 'page-loader-enabled'}`}>
+      <div className="page-loader-inner" />
     </div>
   );
 };
