e58839d79bc7fb6bb378ee6185a4a9245a8d161b
VLadislav Samukhin <vsamukhin@gmail.com>
2023-11-02 15:58:12 +0600

    TPC-29 added getting all guides and one guide

---
+++ backend/routers/guides.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import Guide from "../models/Guide";
+
+
+const guidesRouter = express.Router();
+
+guidesRouter.get("/" , async (req, res) => {
+    try {
+        const guides = await Guide.find().populate({path: "user", select: "username , displayName"});
+        return res.send(guides);
+    } catch (e) {
+        return res.status(500).send("Error");
+    }
+});
+
+guidesRouter.get("/:id" , async (req, res) => {
+   try {
+       const id = req.params.id;
+       const guide = await Guide.findById(id).populate({path: "user", select: "username , displayName"});
+
+       return res.send(guide);
+   } catch (e) {
+       return res.status(500).send("Error");
+   }
+});
