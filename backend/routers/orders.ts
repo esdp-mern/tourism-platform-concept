1b830fd207dcdb3ab87a8cff33578fb2d0f36c75
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-11-08 18:14:31 +0600

    TPC-37: added post route for orders

--- backend/routers/orders.ts
+++ backend/routers/orders.ts
@@ -1,11 +1,35 @@
 import express from 'express';
 import Order from '../models/Order';
+import auth, { RequestWithUser } from '../middleware/auth';
+import mongoose from 'mongoose';
 
 const ordersRouter = express.Router();
 
 ordersRouter.get('/', async (req, res) => {
   const orders = await Order.find();
   return res.send(orders);
+});
+
+ordersRouter.post('/', auth, async (req, res, next) => {
+  try {
+    const user = (req as RequestWithUser).user;
+
+    const order = new Order({
+      user: user._id,
+      guide: req.body.guide,
+      tour: req.body.tour,
+      price: req.body.price,
+      date: req.body.date,
+    });
+
+    await order.save();
+    return res.send(order);
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(e);
+    }
+    return next(e);
+  }
 });
 
 export default ordersRouter;
