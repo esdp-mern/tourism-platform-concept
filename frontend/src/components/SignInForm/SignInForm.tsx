722db786d640004fcc3f91e135f5b8e418a9911e
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-11-08 22:38:43 +0600

    TPC-50: added avatar for googleId

--- frontend/src/components/SignInForm/SignInForm.tsx
+++ frontend/src/components/SignInForm/SignInForm.tsx
@@ -48,7 +48,6 @@ const SignInForm = () => {
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <GoogleLogin
             onSuccess={(credentialResponse) => {
-              console.log(credentialResponse);
               if (credentialResponse.credential) {
                 void googleLoginHandler(credentialResponse.credential);
               }
