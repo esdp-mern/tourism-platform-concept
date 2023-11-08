9a64cf8565ecf9811c4eea124fda7fe06618fb48
Aiankhan Koichubakov <akoychubakov@gmail.com>
2023-11-09 03:42:56 +0600

    TPC-41: Create delete, put, patch requests, fixed post request, added gitignore for images.

--- backend/routers/tours.ts
+++ backend/routers/tours.ts
@@ -14,9 +14,9 @@ toursRouter.get('/', async (req, res) => {
     let tours;
 
     if (req.query.guide) {
-      tours = await Tour.find({guid: req.query.guide, isPublished: true });
+      tours = await Tour.find({ guid: req.query.guide, isPublished: true });
     } else {
-      tours = await Tour.find();
+      tours = await Tour.find({ isPublished: true });
     }
 
     const formattedTours = await Promise.all(
@@ -51,7 +51,6 @@ toursRouter.get('/', async (req, res) => {
   }
 });
 
-
 toursRouter.get('/:id', async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id).populate({
@@ -66,14 +65,7 @@ toursRouter.get('/:id', async (req, res) => {
       return res.status(404).send('Not found!');
     }
 
-    let rating = 0;
-
     const reviews = await Review.find({ tour: tour._id });
-
-    if (reviews.length > 0) {
-      rating =
-        reviews.reduce((acc, value) => acc + value.rating, 0) / reviews.length;
-    }
 
     const tourReviews = {
       _id: tour._id,
@@ -93,8 +85,7 @@ toursRouter.get('/:id', async (req, res) => {
       country: tour.country,
       galleryTour: tour.galleryTour,
       isPublished: tour.isPublished,
-      commentTour: reviews,
-      ratingTour: rating,
+      reviews: reviews,
     };
     return res.send(tourReviews);
   } catch (e) {
@@ -104,31 +95,42 @@ toursRouter.get('/:id', async (req, res) => {
 
 toursRouter.post('/', auth, permit('admin'), upload.fields([{ name: 'mainImage', maxCount: 1 }, { name: 'galleryTour', maxCount: 10 }]), async (req, res, next) => {
   try {
-    const guidePromises = req.body.guide.map(async (guideId: string) => await Guide.findById(guideId));
-    const guides = await Promise.all(guidePromises);
+    let existGuide;
 
-    if (guides.some((guide) => !guide)) {
-      return res.status(404).send('One or more guides do not exist');
+    if (req.body.guide) {
+      const guide = JSON.parse(req.body.guide);
+
+      existGuide = await Promise.all(
+          guide.map(async (guide: string) => {
+            const guides = await Guide.findById(guide);
+
+            if (guides) {
+              return guides._id;
+            }
+          })
+      );
     }
 
-    const guide = guides.map((guide) => guide._id);
     const mainImage = req.files && 'mainImage' in req.files ? req.files['mainImage'][0].filename : null;
     const gallery = req.files && 'galleryTour' in req.files ? req.files['galleryTour'].map((file: Express.Multer.File) => file.filename) : [];
+    const plan = JSON.parse(req.body.plan);
+    const category = JSON.parse(req.body.category);
+    const included = JSON.parse(req.body.included);
 
     const tour = new Tour({
-      guide: guide,
+      guide: existGuide,
       name: req.body.name,
       mainImage: mainImage,
       description: req.body.description,
-      category: req.body.category,
-      price: parseFloat(req.body.price),
+      category: category,
+      price: req.body.price,
       duration: req.body.duration,
-      plan: req.body.plan,
+      plan: plan,
       destination: req.body.destination,
       arrival: req.body.arrival,
       departure: req.body.departure,
       dressCode: req.body.dressCode,
-      included: req.body.included,
+      included: included,
       galleryTour: gallery,
       country: req.body.country,
     });
@@ -140,6 +142,100 @@ toursRouter.post('/', auth, permit('admin'), upload.fields([{ name: 'mainImage',
       return res.status(400).send(e);
     }
     return next(e);
+  }
+});
+
+toursRouter.put('/:id', auth, permit('admin'), upload.fields([{ name: 'mainImage', maxCount: 1 }, { name: 'galleryTour', maxCount: 10 }]), async (req, res, next) => {
+  try {
+    const tourId = req.params.id;
+
+    const existingTour = await Tour.findById(tourId);
+
+    if (!existingTour) {
+      return res.status(404).send('Tour not found');
+    }
+
+    let existGuide;
+
+    if (req.body.guide) {
+      const guide = JSON.parse(req.body.guide);
+
+      existGuide = await Promise.all(
+          guide.map(async (guideId: string) => {
+            const guides = await Guide.findById(guideId);
+
+            if (guides) {
+              return guides._id;
+            }
+          })
+      );
+    }
+
+    const mainImage = req.files && 'mainImage' in req.files ? req.files['mainImage'][0].filename : existingTour.mainImage;
+    const gallery = req.files && 'galleryTour' in req.files ? req.files['galleryTour'].map((file: Express.Multer.File) => file.filename) : existingTour.galleryTour;
+    const plan = req.body.plan ? JSON.parse(req.body.plan) : existingTour.plan;
+    const category = req.body.category ? JSON.parse(req.body.category) : existingTour.category;
+    const included = req.body.included ? JSON.parse(req.body.included) : existingTour.included;
+
+    existingTour.guide = existGuide || existingTour.guide;
+    existingTour.name = req.body.name || existingTour.name;
+    existingTour.mainImage = mainImage;
+    existingTour.description = req.body.description || existingTour.description;
+    existingTour.category = category;
+    existingTour.price = req.body.price || existingTour.price;
+    existingTour.duration = req.body.duration || existingTour.duration;
+    existingTour.plan = plan;
+    existingTour.destination = req.body.destination || existingTour.destination;
+    existingTour.arrival = req.body.arrival || existingTour.arrival;
+    existingTour.departure = req.body.departure || existingTour.departure;
+    existingTour.dressCode = req.body.dressCode || existingTour.dressCode;
+    existingTour.included = included;
+    existingTour.galleryTour = gallery;
+    existingTour.country = req.body.country || existingTour.country;
+
+    await existingTour.save();
+
+    return res.send(existingTour);
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(e);
+    }
+    return next(e);
+  }
+});
+
+toursRouter.delete('/:id', auth, permit('admin'), async (req, res, next) => {
+  try {
+    const tourId = req.params.id;
+
+    const existingTour = await Tour.findById(tourId);
+
+    if (!existingTour) {
+      return res.status(404).send('Tour not found');
+    }
+
+    await existingTour.deleteOne();
+
+    return res.send('Tour deleted successfully');
+  } catch (e) {
+    return next(e);
+  }
+});
+
+toursRouter.patch('/:id/togglePublished', auth, permit('admin'), async (req, res) => {
+  try {
+    const id = req.params.id;
+    const tour = await Tour.findById(id);
+
+    if (!tour) {
+      return res.status(404).send('Not Found!');
+    }
+
+    await Tour.findByIdAndUpdate(id, { isPublished: !tour.isPublished });
+
+    res.send('Changed');
+  } catch (e) {
+    res.status(500).send(e);
   }
 });
 
