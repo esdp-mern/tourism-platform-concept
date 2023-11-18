aa791000f3b9883df2b5ecf217c6bf7b08e03ce0
Bektur Razzakov <brzzkv@gmail.com>
2023-11-18 14:55:06 +0600

    TPC-89: changed tour item clickable parts

--- frontend/src/components/TourListItem/TourListItem.tsx
+++ frontend/src/components/TourListItem/TourListItem.tsx
@@ -29,15 +29,15 @@ const TourItem: React.FC<Props> = ({ tour }) => {
 
   return (
     <Fade>
-      <Link href={`/tours/${tour._id}`} className="tour-item">
-        <div className="tour-item-top">
+      <div className="tour-item">
+        <Link href={`/tours/${tour._id}`} className="tour-item-top">
           <img src={imgLink} alt={tour.name} className="tour-item-img" />
           <div className="tour-item-price">{tour.price.toString()} KGS</div>
-        </div>
+        </Link>
         <div className="tour-item-bottom">
-          <span className="tour-item-links">
+          <Link href={`/tours/${tour._id}`} className="tour-item-links">
             <h2 className="tour-item-title">{tour.name}</h2>
-          </span>
+          </Link>
           <div className="tour-item-country">{tour.country}</div>
           <div className="tour-item-info">
             <div className="tour-item-duration">{tour.duration} days</div>
@@ -66,7 +66,7 @@ const TourItem: React.FC<Props> = ({ tour }) => {
             </div>
           )}
         </div>
-      </Link>
+      </div>
     </Fade>
   );
 };
