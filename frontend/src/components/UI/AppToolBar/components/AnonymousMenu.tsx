e56686b11be90115924ac7b51951a74f8e1d01e5
VLadislav Samukhin <vsamukhin@gmail.com>
2023-11-22 20:46:39 +0600

    TPC-14 added style login and register forms

--- frontend/src/components/UI/AppToolBar/components/AnonymousMenu.tsx
+++ frontend/src/components/UI/AppToolBar/components/AnonymousMenu.tsx
@@ -4,9 +4,6 @@ import NavLink from 'next/link';
 const AnonymousMenu = () => {
   return (
     <>
-      <NavLink href="/register" className="nav-link">
-        Sign up
-      </NavLink>
       <NavLink href="/login" className="nav-link">
         Sign in
       </NavLink>
