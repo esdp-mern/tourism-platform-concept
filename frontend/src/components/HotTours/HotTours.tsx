6397eeaff5072e4aec711ab895c362979ca10336
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-11-24 02:31:12 +0600

    TPC-58: added carousel for hot tours

--- frontend/src/components/HotTours/HotTours.tsx
+++ frontend/src/components/HotTours/HotTours.tsx
@@ -1,17 +1,107 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useAppSelector } from '@/store/hooks';
 import { selectAllTours } from '@/containers/tours/toursSlice';
 import HotToursItem from '@/components/HotTours/components/HotToursItem/HotToursItem';
+import arrowRightIcon from '@/assets/images/arrow-right.svg';
+import { Tour } from '@/type';
+
+const x = 445;
 
 const HotTours = () => {
   const tours = useAppSelector(selectAllTours);
+  const carouselRef = useRef<HTMLDivElement | null>(null);
+  const [carouselTours, setCarouselTours] = useState<Tour[]>(
+    tours.length ? [tours[0], tours[1], tours[2], tours[3]] : [],
+  );
 
-  const tour = tours[0];
+  const slide = useCallback(
+    (isNext: boolean) => {
+      if (!carouselRef.current && !carouselTours.length) return;
+
+      const ms = 250;
+
+      carouselRef.current!.style.cssText = `
+          transition: all ${ms}ms ease 0s;
+          transform: translateX(${isNext ? '-' : ''}${x}px);
+        `;
+
+      setTimeout(() => {
+        carouselRef.current!.style.cssText = `
+            transition: none;
+            transform: translateX(0);
+          `;
+
+        setCarouselTours((prevState) => {
+          const newState = [...prevState];
+
+          const lastOrFirstCarouselTour =
+            newState[isNext ? newState.length - 1 : 0];
+
+          if (isNext) {
+            newState.shift();
+          } else {
+            newState.pop();
+          }
+
+          let lastOrFirstTour = tours.findIndex(
+            (tour) => tour._id === lastOrFirstCarouselTour._id,
+          );
+
+          if (isNext) {
+            if (lastOrFirstTour === tours.length - 1) {
+              lastOrFirstTour = -1;
+            }
+            newState.push(tours[lastOrFirstTour + 1]);
+          } else {
+            if (lastOrFirstTour === 0) {
+              lastOrFirstTour = tours.length - 1;
+            }
+            newState.unshift(tours[lastOrFirstTour - 1]);
+          }
+
+          return newState;
+        });
+      }, ms);
+    },
+    [carouselTours.length, tours],
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => slide(true), 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [slide]);
 
   return (
     <div className="hot-tours-wrapper">
       <div className="container hot-tours-wrapper-inner">
-        <HotToursItem tour={tour} />
+        <div className="hot-tours-carousel">
+          <div className="hot-tours-carousel-inner-wrapper">
+            <div className="hot-tours-carousel-inner">
+              <div className="hot-tours-carousel-inner-items" ref={carouselRef}>
+                {carouselTours.map((tour) => (
+                  <HotToursItem tour={tour} key={`hot-tour-${tour._id}`} />
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="hot-tours-carousel-buttons">
+            <button
+              className="hot-tours-carousel-buttons-next"
+              onClick={() => slide(true)}
+            >
+              <img src={arrowRightIcon.src} alt="arrow-right-icon" />
+            </button>
+            <button
+              className="hot-tours-carousel-buttons-prev"
+              onClick={() => slide(false)}
+            >
+              <img src={arrowRightIcon.src} alt="arrow-left-icon" />
+            </button>
+          </div>
+        </div>
         <div className="hot-tours-info">
           <div>
             <h2>
--- frontend/src/components/HotTours/components/HotToursItem/HotToursItem.tsx
+++ frontend/src/components/HotTours/components/HotToursItem/HotToursItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tour } from '@/type';
 import { apiUrl } from '@/constants';
+import NavLink from 'next/link';
 
 interface Props {
   tour: Tour;
@@ -9,10 +10,25 @@ interface Props {
 const HotToursItem: React.FC<Props> = ({ tour }) => {
   return (
     <div className="hot-tours-item">
-      <img src={apiUrl + '/' + tour.mainImage} alt="..." />
-      <div>
-        <h2>{tour.name}</h2>
-        <p>{tour.price}</p>
+      <NavLink href={`/tours/${tour._id}`} className="hot-tours-item-preview">
+        <img src={apiUrl + '/' + tour.mainImage} alt="..." />
+        <div className="hot-tours-item-badge">-30%</div>
+      </NavLink>
+      <div className="hot-tours-item-info">
+        <p className="hot-tours-item-title">
+          <NavLink
+            href={`/tours/${tour._id}`}
+            className="hot-tours-item-title-link"
+          >
+            {tour.name}
+          </NavLink>
+        </p>
+        <div className="hot-tours-item-pricing">
+          <p className="hot-tours-item-price hot-tours-item-price-old">$9999</p>
+          <p className="hot-tours-item-price hot-tours-item-price-new">
+            ${tour.price}
+          </p>
+        </div>
       </div>
     </div>
   );
