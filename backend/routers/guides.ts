70fa50d53f30da220e4b63ff9864dcf05e4b0463
VLadislav Samukhin <vsamukhin@gmail.com>
2023-12-01 00:37:07 +0600

    TPC-66 fixed request for moderators and changed requests for guides

--- backend/routers/guides.ts
+++ backend/routers/guides.ts
@@ -7,23 +7,9 @@ guidesRouter.get('/', async (req, res) => {
   try {
     const guides = await Guide.find().populate({
       path: 'user',
-      select: 'username , displayName',
+      select: 'username , displayName , avatar',
     });
 
-    // const guidesWithRating = guides.map(async (guide) => {
-    //   let rating = 0;
-    //   const guideReviews = await TourReview.find({ guide: guide._id });
-    //
-    //   if (guideReviews.length > 0) {
-    //     rating =
-    //       guideReviews.reduce((acc, value) => acc + value.rating, 0) /
-    //       guideReviews.length;
-    //   }
-    //
-    //   return { ...guide.toObject(), rating };
-    // });
-    //
-    // const guidesAll = await Promise.all(guidesWithRating);
     return res.send(guides);
   } catch (e) {
     return res.status(500).send('Error');
@@ -41,17 +27,6 @@ guidesRouter.get('/:id', async (req, res) => {
     if (!guide) {
       return res.status(404).send('Not found');
     }
-
-    // let rating = 0;
-    // const guideReviews = await TourReview.find({ guide: guide._id });
-    //
-    // if (guideReviews.length > 0) {
-    //   rating =
-    //     guideReviews.reduce((acc, value) => acc + value.rating, 0) /
-    //     guideReviews.length;
-    // }
-    //
-    // const guideWithRating = { ...guide.toObject(), rating };
 
     return res.send(guide);
   } catch (e) {
