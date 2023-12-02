1ef5070b40171d3851eca974fe2e1af8d552f18e
sakutaku <116337732+sakutaku@users.noreply.github.com>
2023-12-02 16:29:40 +0600

    TPC-92 added hot tours to navbar

--- frontend/src/components/OneTourPage/OneTourInformation/OneTourInformation.tsx
+++ frontend/src/components/OneTourPage/OneTourInformation/OneTourInformation.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Fade } from 'react-awesome-reveal';
-import { selectOneTour } from '@/containers/tours/toursSlice';
+import {
+  galleryModal,
+  selectOneTour,
+  showModal,
+} from '@/containers/tours/toursSlice';
 import { apiUrl } from '@/constants';
-import { useAppSelector } from '@/store/hooks';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import GalleryItem from '@/components/OneTourPage/Gallery/GalleryItem';
+import GalleryModal from '@/components/OneTourPage/Gallery/GalleryModal';
 
 const OneTourInformation = () => {
   const tour = useAppSelector(selectOneTour);
+  const modal = useAppSelector(galleryModal);
+  const dispatch = useAppDispatch();
+  const [currentImg, setCurrentImg] = useState('');
 
   const getSpan = (value: string) => <span>{value}</span>;
 
@@ -15,81 +24,144 @@ const OneTourInformation = () => {
     `${tour.duration} Day${tour.duration > 1 ? 's' : ''}`,
   );
 
+  const onOpenModal = (e: React.MouseEvent) => {
+    const src = e.currentTarget.getAttribute('src')!;
+    setCurrentImg(src);
+    dispatch(showModal(true));
+  };
+
+  const onGalleryImgsClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    const src = e.currentTarget.getAttribute('src')!;
+    setCurrentImg(src);
+  };
+  const onLeftClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+
+    const src = e.currentTarget.getAttribute('src')!;
+    setCurrentImg(src);
+    let index = 0;
+
+    tour?.galleryTour.forEach((t, i) => {
+      if (currentImg === apiUrl + '/' + t) {
+        index = i - 1;
+      }
+    });
+
+    if (index >= 0) {
+      setCurrentImg(apiUrl + '/' + tour?.galleryTour[index]);
+    } else {
+      setCurrentImg(currentImg);
+    }
+  };
+
+  const onRightClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+
+    const src = e.currentTarget.getAttribute('src')!;
+    setCurrentImg(src);
+    let index = 0;
+
+    tour?.galleryTour.forEach((t, i) => {
+      if (currentImg === apiUrl + '/' + t) {
+        index = i + 1;
+      }
+    });
+
+    if (index >= 0 && index < tour?.galleryTour.length!) {
+      setCurrentImg(apiUrl + '/' + tour?.galleryTour[index]);
+    } else {
+      setCurrentImg(currentImg);
+    }
+  };
+
   return (
-    <Fade>
-      <div className="one-tour-inner">
-        <h3 className="one-tour-inner-title">{tour.name}</h3>
-        <div className="one-tour-inner-price-wrap">
-          <span className="one-tour-inner-price">{tour.price} KGS</span>
-          per person
-        </div>
-        <div className="one-tour-inner-info">
-          <div className="one-tour-inner-duration">{duration}</div>
-          <div className="one-tour-inner-featur">Featured Tour</div>
-        </div>
-        <div className="one-tour-inner-txt">{tour.description}</div>
-        <div className="one-tour-inner-box">
-          <table className="one-tour-inner-table">
-            <tbody>
-              <tr>
-                <td>Destination</td>
-                <td>{tour.country}</td>
-              </tr>
-              <tr>
-                <td>Arrival</td>
-                <td>{tour.arrival}</td>
-              </tr>
-              <tr>
-                <td>Departure</td>
-                <td>{tour.departure}</td>
-              </tr>
-              <tr>
-                <td>What is Included</td>
-                <td>
-                  <ul className="one-tour-inner-table-list">
-                    {tour.included.map((inc, id) => (
-                      <li key={id}>{inc}</li>
+    <>
+      <Fade>
+        <div className="one-tour-inner">
+          <h3 className="one-tour-inner-title">{tour.name}</h3>
+          <div className="one-tour-inner-price-wrap">
+            <span className="one-tour-inner-price">{tour.price} KGS</span>
+            per person
+          </div>
+          <div className="one-tour-inner-info">
+            <div className="one-tour-inner-duration">{duration}</div>
+            <div className="one-tour-inner-featur">Featured Tour</div>
+          </div>
+          <div className="one-tour-inner-txt">{tour.description}</div>
+          <div className="one-tour-inner-box">
+            <table className="one-tour-inner-table">
+              <tbody>
+                <tr>
+                  <td>Destination</td>
+                  <td>{tour.country}</td>
+                </tr>
+                <tr>
+                  <td>Arrival</td>
+                  <td>{tour.arrival}</td>
+                </tr>
+                <tr>
+                  <td>Departure</td>
+                  <td>{tour.departure}</td>
+                </tr>
+                <tr>
+                  <td>What is Included</td>
+                  <td>
+                    <ul className="one-tour-inner-table-list">
+                      {tour.included.map((inc, id) => (
+                        <li key={id}>{inc}</li>
+                      ))}
+                    </ul>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Departure</td>
+                  <td>{tour.dressCode}</td>
+                </tr>
+                <tr>
+                  <td>Guides</td>
+                  <td>
+                    {tour.guides.map((guide) => (
+                      <div key={guide._id}>{guide.user.displayName}</div>
                     ))}
-                  </ul>
-                </td>
-              </tr>
-              <tr>
-                <td>Departure</td>
-                <td>{tour.dressCode}</td>
-              </tr>
-              <tr>
-                <td>Guides</td>
-                <td>
-                  {tour.guides.map((guide) => (
-                    <div key={guide._id}>{guide.user.displayName}</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Category</td>
+                  {tour.category.map((cat, id) => (
+                    <td key={id}>{cat}</td>
                   ))}
-                </td>
-              </tr>
-              <tr>
-                <td>Category</td>
-                {tour.category.map((cat, id) => (
-                  <td key={id}>{cat}</td>
-                ))}
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="one-tour-inner-gallery">
-          <h3>Tour Gallery</h3>
-          <div className="one-tour-inner-gallery2">
-            {tour.galleryTour.map((photo, id) => (
-              <div key={id}>
-                <img
-                  alt="photo"
-                  src={apiUrl + '/' + photo}
-                  className="one-tour-inner-photo"
-                />
-              </div>
-            ))}
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="one-tour-inner-gallery">
+            <h3>Tour Gallery</h3>
+            <div className="one-tour-inner-gallery2">
+              {/*{tour.galleryTour.map((photo, id) => (*/}
+              {/*  <div key={id}>*/}
+              {/*    <img*/}
+              {/*      alt="photo"*/}
+              {/*      src={apiUrl + '/' + photo}*/}
+              {/*      className="one-tour-inner-photo"*/}
+              {/*    />*/}
+              {/*  </div>*/}
+              {/*))}*/}
+              <GalleryItem tour={tour} onOpenModal={onOpenModal} />
+            </div>
           </div>
         </div>
-      </div>
-    </Fade>
+      </Fade>
+      {modal ? (
+        <GalleryModal
+          tour={tour}
+          currentImg={currentImg}
+          onLeftClick={onLeftClick}
+          onRightClick={onRightClick}
+          onGalleryImgsClick={onGalleryImgsClick}
+        />
+      ) : null}
+    </>
   );
 };
 
