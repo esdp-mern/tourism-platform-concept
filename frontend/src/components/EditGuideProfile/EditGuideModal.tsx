5b12e0da0f849cc22a305e8e5d76fe8393059014
VLadislav Samukhin <vsamukhin@gmail.com>
2023-12-17 15:23:44 +0700

    TPC-141 added page , styles and  components in profile

---
+++ frontend/src/components/EditGuideProfile/EditGuideModal.tsx
@@ -0,0 +1,116 @@
+import React, { FormEvent, useEffect, useState } from 'react';
+import {
+  addAlert,
+  selectUser,
+  setEditorModal,
+} from '@/containers/users/usersSlice';
+import TextField from '@/components/UI/TextField/TextField';
+import penIcon from '@/assets/images/pen-icon.svg';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { IChangeEvent } from '@/components/OneTourOrderForm/OneTourOrderForm';
+import { AxiosError } from 'axios';
+import { IEditGuide } from '@/type';
+import {
+  selectEditGuideLoading,
+  selectEditorGuideModal,
+  selectGuideUser,
+  setGuideEditorModal,
+} from '@/containers/guides/guidesSlice';
+import { editGuide, fetchGuideUser } from '@/containers/guides/guidesThunk';
+import ButtonLoader from '@/components/Loaders/ButtonLoader';
+
+const EditGuideModal = () => {
+  const dispatch = useAppDispatch();
+  const guide = useAppSelector(selectGuideUser);
+  const user = useAppSelector(selectUser);
+  const editLoading = useAppSelector(selectEditGuideLoading);
+  const [state, setSate] = useState<IEditGuide>({
+    id: '',
+    description: '',
+    languages: [],
+    country: '',
+  });
+  const modal = useAppSelector(selectEditorGuideModal);
+
+  useEffect(() => {
+    if (!guide) return;
+    setSate((prevState) => ({
+      ...prevState,
+      id: guide._id,
+      description: guide.description,
+      country: guide.country,
+      languages: guide.languages,
+    }));
+  }, [guide]);
+
+  const onChangeHandler = (e: IChangeEvent) => {
+    const { name, value } = e.target;
+    if (name === 'languages') {
+      setSate((prevState) => ({
+        ...prevState,
+        languages: value.split(',').map((str) => str),
+      }));
+    }
+    setSate((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      await dispatch(editGuide(state)).unwrap();
+      await dispatch(fetchGuideUser(user._id));
+      dispatch(addAlert({ message: 'Changes are saved', type: 'info' }));
+      dispatch(setGuideEditorModal());
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        dispatch(addAlert({ message: 'Something is wrong!', type: 'error' }));
+      }
+    }
+  };
+
+  return (
+    <div
+      className={`editor-modal ${modal ? 'editor-modal-open' : ''}`}
+      onClick={(e) => {
+        e.stopPropagation();
+        dispatch(setGuideEditorModal());
+      }}
+    >
+      <div className="editor-modal-inner" onClick={(e) => e.stopPropagation()}>
+        <form onSubmit={onSubmit}>
+          <h2>Edit description</h2>
+          <textarea
+            className="guide-input-description"
+            name="description"
+            value={state.description}
+            onChange={onChangeHandler}
+            required
+          />
+          <TextField
+            name="country"
+            type="text"
+            value={state.country}
+            onChange={onChangeHandler}
+            icon={penIcon.src}
+            label="country"
+            required
+          />
+          <TextField
+            name="languages"
+            type="text"
+            value={state.languages.toString()}
+            onChange={onChangeHandler}
+            icon={penIcon.src}
+            label="languages"
+            required
+          />
+          <button type="submit" className="form-tour-btn" style={{ margin: 0 }}>
+            {editLoading ? <ButtonLoader size={18} /> : 'Save'}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditGuideModal;
---
+++ frontend/src/components/UserOrders/UserOrders.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { useAppSelector } from '@/store/hooks';
+import { selectOrdersUser } from '@/containers/orders/ordersSlice';
+import dayjs from 'dayjs';
+
+const UserOrders = () => {
+  const orders = useAppSelector(selectOrdersUser);
+
+  return (
+    <div className="orders-list">
+      {orders.length > 0 ? (
+        orders.map((order) => (
+          <div key={order._id} className="orders">
+            <h4 className="orders_tour">Tour: {order.tour.name}</h4>
+            <p className="orders_guide">
+              Guide: {order.guide.user.displayName}
+            </p>
+            <p className="orders_datetime">
+              Date: {dayjs(order.datetime).format('DD.MM.YY HH:MM') || '-'}
+            </p>
+            <p className="orders_date">
+              Date tour: {dayjs(order.date).format('DD.MM.YY HH:MM') || '-'}
+            </p>
+            <p className="orders_price">Price: {order.price}KGS</p>
+            <div className="orders_status">Status: {order.status}</div>
+          </div>
+        ))
+      ) : (
+        <span>No orders</span>
+      )}
+      {orders.length > 0 ? (
+        orders.map((order) => (
+          <div key={order._id + '3'} className="orders">
+            <h4 className="orders_tour">Tour: {order.tour.name}</h4>
+            <p className="orders_guide">
+              Guide: {order.guide.user.displayName}
+            </p>
+            <p className="orders_datetime">
+              Date: {dayjs(order.datetime).format('DD.MM.YY HH:MM') || '-'}
+            </p>
+            <p className="orders_date">
+              Date tour: {dayjs(order.date).format('DD.MM.YY HH:MM') || '-'}
+            </p>
+            <p className="orders_price">Price: {order.price}KGS</p>
+            <div className="orders_status">Status: {order.status}</div>
+          </div>
+        ))
+      ) : (
+        <span>No orders</span>
+      )}
+      {orders.length > 0 ? (
+        orders.map((order) => (
+          <div key={order._id + '2'} className="orders">
+            <h4 className="orders_tour">Tour: {order.tour.name}</h4>
+            <p className="orders_guide">
+              Guide: {order.guide.user.displayName}
+            </p>
+            <p className="orders_datetime">
+              Date: {dayjs(order.datetime).format('DD.MM.YY HH:MM') || '-'}
+            </p>
+            <p className="orders_date">
+              Date tour: {dayjs(order.date).format('DD.MM.YY HH:MM') || '-'}
+            </p>
+            <p className="orders_price">Price: {order.price}KGS</p>
+            <div className="orders_status">Status: {order.status}</div>
+          </div>
+        ))
+      ) : (
+        <span>No orders</span>
+      )}
+      {orders.length > 0 ? (
+        orders.map((order) => (
+          <div key={order._id + '1'} className="orders">
+            <h4 className="orders_tour">Tour: {order.tour.name}</h4>
+            <p className="orders_guide">
+              Guide: {order.guide.user.displayName}
+            </p>
+            <p className="orders_datetime">
+              Date: {dayjs(order.datetime).format('DD.MM.YY HH:MM') || '-'}
+            </p>
+            <p className="orders_date">
+              Date tour: {dayjs(order.date).format('DD.MM.YY HH:MM') || '-'}
+            </p>
+            <p className="orders_price">Price: {order.price}KGS</p>
+            <div className="orders_status">Status: {order.status}</div>
+          </div>
+        ))
+      ) : (
+        <span>No orders</span>
+      )}
+    </div>
+  );
+};
+
+export default UserOrders;
