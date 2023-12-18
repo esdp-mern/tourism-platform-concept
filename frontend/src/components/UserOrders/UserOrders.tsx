36dfbdfdcd9b8581f799dfe2ba5e2f7501ad1c5d
sakutaku <saikalabytova1@gmail.com>
2023-12-18 14:59:23 +0600

    TPC-141 added some changes

--- frontend/src/components/UserOrders/UserOrders.tsx
+++ frontend/src/components/UserOrders/UserOrders.tsx
@@ -28,66 +28,6 @@ const UserOrders = () => {
       ) : (
         <span>No orders</span>
       )}
-      {orders.length > 0 ? (
-        orders.map((order) => (
-          <div key={order._id + '3'} className="orders">
-            <h4 className="orders_tour">Tour: {order.tour.name}</h4>
-            <p className="orders_guide">
-              Guide: {order.guide.user.displayName}
-            </p>
-            <p className="orders_datetime">
-              Date: {dayjs(order.datetime).format('DD.MM.YY HH:MM') || '-'}
-            </p>
-            <p className="orders_date">
-              Date tour: {dayjs(order.date).format('DD.MM.YY HH:MM') || '-'}
-            </p>
-            <p className="orders_price">Price: {order.price}KGS</p>
-            <div className="orders_status">Status: {order.status}</div>
-          </div>
-        ))
-      ) : (
-        <span>No orders</span>
-      )}
-      {orders.length > 0 ? (
-        orders.map((order) => (
-          <div key={order._id + '2'} className="orders">
-            <h4 className="orders_tour">Tour: {order.tour.name}</h4>
-            <p className="orders_guide">
-              Guide: {order.guide.user.displayName}
-            </p>
-            <p className="orders_datetime">
-              Date: {dayjs(order.datetime).format('DD.MM.YY HH:MM') || '-'}
-            </p>
-            <p className="orders_date">
-              Date tour: {dayjs(order.date).format('DD.MM.YY HH:MM') || '-'}
-            </p>
-            <p className="orders_price">Price: {order.price}KGS</p>
-            <div className="orders_status">Status: {order.status}</div>
-          </div>
-        ))
-      ) : (
-        <span>No orders</span>
-      )}
-      {orders.length > 0 ? (
-        orders.map((order) => (
-          <div key={order._id + '1'} className="orders">
-            <h4 className="orders_tour">Tour: {order.tour.name}</h4>
-            <p className="orders_guide">
-              Guide: {order.guide.user.displayName}
-            </p>
-            <p className="orders_datetime">
-              Date: {dayjs(order.datetime).format('DD.MM.YY HH:MM') || '-'}
-            </p>
-            <p className="orders_date">
-              Date tour: {dayjs(order.date).format('DD.MM.YY HH:MM') || '-'}
-            </p>
-            <p className="orders_price">Price: {order.price}KGS</p>
-            <div className="orders_status">Status: {order.status}</div>
-          </div>
-        ))
-      ) : (
-        <span>No orders</span>
-      )}
     </div>
   );
 };
