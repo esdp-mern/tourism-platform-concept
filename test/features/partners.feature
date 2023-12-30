34d147054119445f85588d99869f6340e1ad687c
Meerim K <thvrim@gmail.com>
2023-12-31 01:57:58 +0600

    TPC-168 added deleting partner test

--- test/features/partners.feature
+++ test/features/partners.feature
@@ -2,7 +2,7 @@
 
 Feature: Partner tests for Admin
 
-  Scenario: Admin creates a partner
+  Scenario: Admin creates/edit/delete a partner
     Given I am on sign in page
     When I enter form fields:
       | username | admin      |
@@ -12,4 +12,14 @@ Feature: Partner tests for Admin
     When I click "Admin Page" link
     Then I go to the partner accepting page
     When I click the first "Accept" button
-    Then I go to the home page
+    Then I go to the admin partner page
+#    When I click the first "Edit" link
+#    When I navigate to the partner edit page
+#    When I enter new partner form fields:
+#      | name | Partner test |
+#    Then I click "Save" button
+#    Then I go to the admin partner page
+    When I click the first "Delete" button
+    Then I see the confirmation alert "Are you sure you want to delete this partner?"
+    Then I confirm the deletion by clicking "ok" in the confirmation alert
+    Then I stay on the partner admin page
--- test/step_definitions/partnersSteps.ts
+++ test/step_definitions/partnersSteps.ts
@@ -1,39 +1,78 @@
 import assert from "assert";
 
-const { I } = inject();
+const {I} = inject();
 
 Given("I am on sign in page", () => {
-  I.amOnPage("/login");
-  I.wait(1);
+    I.amOnPage("/login");
+    I.wait(1);
 });
 
 When("I enter form fields:", (userData) => {
-  userData.rows.forEach((row) => {
-    const [fieldName, fieldValue] = row.cells;
-    I.fillField(fieldName.value, fieldValue.value);
-  });
+    userData.rows.forEach((row) => {
+        const [fieldName, fieldValue] = row.cells;
+        I.fillField(fieldName.value, fieldValue.value);
+    });
 });
 
 When("I click {string} button", (text: string) => {
-  I.click(`//button[contains(text(), '${text}')]`);
-  I.wait(3);
+    I.click(`//button[contains(text(), '${text}')]`);
+    I.wait(3);
+});
+
+When('I click "Save" button', async () => {
+    // Wait for the "Save" button to be clickable
+    I.waitForClickable('//button[contains(text(), "Save")]', 10);
+    I.click('//button[contains(text(), "Save")]');
+    I.wait(3);
 });
 
 When("I click {string} link", (text: string) => {
-  I.click(`//a[contains(text(), '${text}')]`);
+    I.click(`//a[contains(text(), '${text}')]`);
 });
 
 Then("I go to the partner accepting page", () => {
-  I.amOnPage("/admin/partnerOrders/1");
-  I.wait(5);
+    I.amOnPage("/admin/partnerOrders/1");
+    I.wait(5);
 });
 
 When('I click the first "Accept" button', async () => {
-  I.click('//button[contains(text(), "Accept")][1]');
-  I.wait(3);
+    I.click('//button[contains(text(), "Accept")][1]');
+    I.wait(3);
 });
 
 Then('I go to the home page', () => {
-  I.amOnPage("/");
-  I.wait(4);
+    I.amOnPage("/");
+    I.wait(4);
 });
+
+Then('I go to the admin partner page', () => {
+    I.amOnPage("/admin/partners/all");
+    I.wait(5);
+});
+
+When('I navigate to the partner edit page', async () => {
+    const currentUrl = await I.grabCurrentUrl();
+
+    const parts = currentUrl.split("/");
+    const id = parts[parts.length - 1];
+    I.amOnPage(`/partners/edit/${id}`);
+    I.wait(3);
+});
+When(
+    "I attach the partner file {string} to the {string} input",
+    (filePath, inputSelector) => {
+        I.attachFile(inputSelector, filePath);
+    },
+);
+Then('I stay on the partner admin page', () => {
+    I.amOnPage("/admin/partners/all");
+    I.wait(5);
+});
+When("I enter new partner form fields:", (userData) => {
+    userData.rows.forEach((row) => {
+        const [fieldName, fieldValue] = row.cells;
+        I.clearField(fieldName.value);
+        I.fillField(fieldName.value, fieldValue.value);
+    });
+});
+
